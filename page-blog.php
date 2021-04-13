972b4e207e1f2f1b6bfe3fb5a7b50a114aec5a45
arsua-greg <gregarsua@bpoc.co.jp>
2021-04-13 14:22:04 +0800

    add blog article

--- page-blog.php
+++ page-blog.php
@@ -1,2 +1,37 @@
 <?php get_header(); ?>
+<div class="title">
+    <p>BLOG</p>
+</div>
+<!-- body -->
+
+<section class="blog">
+
+    <div class="content">
+        <?php
+        $paged = (get_query_var('paged')) ? absint(get_query_var('paged')) : 1;
+
+        $args = array(
+            'post_type' => 'post',
+            'post_status' => 'publish',
+            'posts_per_page' => 5,
+            'paged' => $paged,
+        );
+
+        $the_query = new WP_Query($args);
+        ?>
+
+        <?php if ($the_query->have_posts()) : ?>
+
+            <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
+                <div class="r-flex">
+                    <div class="date"><?php echo get_the_date('m.d.Y'); ?></div>
+                    <div class="desc"><a href="<?php echo get_permalink(); ?>"><?php echo the_title(); ?></a></div>
+                    <div class="readmore"><a href="<?php echo get_permalink(); ?>">Read more</a></div>
+                </div>
+                <hr>
+            <?php endwhile; ?>
+        <?php endif; ?>
+    </div>
+
+</section>
 <?php get_footer(); ?>
---
+++ single.php
@@ -0,0 +1,47 @@
+<?php get_header(); ?>
+<section class="blog_article">
+    <div class="container">
+        <div class="r-flex">
+            <div class="left_post">
+                <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                        <img src="<?php echo get_template_directory_uri(); ?>/shared/img/slider_img03.jpg" alt="slide 3" srcset="">
+
+                        <div class="desc">
+                            <h3><?php echo get_the_date('d.m.Y'); ?></h3>
+                            <p>
+                                <?php the_content(); ?>
+                            </p>
+                        </div>
+                    <?php endwhile; ?>
+                <?php else : endif; ?>
+            </div>
+            <div class="center"></div>
+            <div class="right_post">
+                <div class="header">
+                    Latest Post
+                </div>
+                <div class="flex">
+                    <div class="images">
+                        <img src="<?php echo get_template_directory_uri(); ?>/shared/img/access_photo2.jpg" alt="photo1">
+                    </div>
+                    <div class="r-title">劇三ミ図滋すむク郵糖サト転旧モアリヌ見棋閣っ半教にレろ聞発め景問フ舎社レエハノ教方78映へべぎ移判べ延列びやイ。</div>
+                </div>
+                <div class="flex">
+                    <div class="images">
+                        <img src="<?php echo get_template_directory_uri(); ?>/shared/img/access_photo2.jpg" alt="photo1">
+                    </div>
+                    <div class="r-title">劇三ミ図滋すむク郵糖サト転旧モアリヌ見棋閣っ半教にレろ聞発め景問フ舎社レエハノ教方78映へべぎ移判べ延列びやイ。</div>
+                </div>
+                <div class="flex">
+                    <div class="images">
+                        <img src="<?php echo get_template_directory_uri(); ?>/shared/img/access_photo2.jpg" alt="photo1">
+                    </div>
+                    <div class="r-title">劇三ミ図滋すむク郵糖サト転旧モアリヌ見棋閣っ半教にレろ聞発め景問フ舎社レエハノ教方78映へべぎ移判べ延列びやイ。</div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://apps.elfsight.com/p/platform.js" defer></script>
+    <div class="elfsight-app-8f29bef8-5221-495d-8b3d-5ce693d2a4c3"></div>
+</section>
+<?php get_footer(); ?>
